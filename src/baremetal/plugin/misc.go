dc9286bd91cb9bf241675d7897033cbb400773f7
ZhouWenjun <zwjhuhu@126.com>
2020-09-08 12:05:49 +0800

    fix error

--- src/baremetal/plugin/misc.go
+++ src/baremetal/plugin/misc.go
@@ -1,10 +1,8 @@
 package plugin
 
 import (
-	"fmt"
-	"io/ioutil"
 	"baremetal/server"
-	"baremetal/utils"
+	"io/ioutil"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -62,32 +60,32 @@ func GetInitConfig() *InitConfig {
 }
 
 func addRouteIfCallbackIpChanged() {
-	if server.CURRENT_CALLBACK_IP != server.CALLBACK_IP {
-		if server.CURRENT_CALLBACK_IP == "" {
-			log.Debug(fmt.Sprintf("agent first start, add static route to callback ip host"))
-		} else {
-			log.Debug(fmt.Sprintf("detect call back ip host changed, add static route"))
-		}
-		// NOTE(WeiW): Since our mgmt nic is always eth0
-		if server.CURRENT_CALLBACK_IP != "" {
-			err := utils.RemoveVrouterRoute(server.CURRENT_CALLBACK_IP)
-			utils.PanicOnError(err)
-		}
+	// if server.CURRENT_CALLBACK_IP != server.CALLBACK_IP {
+	// 	if server.CURRENT_CALLBACK_IP == "" {
+	// 		log.Debug(fmt.Sprintf("agent first start, add static route to callback ip host"))
+	// 	} else {
+	// 		log.Debug(fmt.Sprintf("detect call back ip host changed, add static route"))
+	// 	}
+	// 	// NOTE(WeiW): Since our mgmt nic is always eth0
+	// 	if server.CURRENT_CALLBACK_IP != "" {
+	// 		err := utils.RemoveVrouterRoute(server.CURRENT_CALLBACK_IP)
+	// 		utils.PanicOnError(err)
+	// 	}
 
-		mgmtNic := utils.GetMgmtInfoFromBootInfo()
-		if mgmtNic == nil || utils.CheckMgmtCidrContainsIp(server.CALLBACK_IP, mgmtNic) == false {
-			err := utils.SetVrouterRoute(server.CALLBACK_IP, "eth0", mgmtNic["gateway"].(string))
-			utils.PanicOnError(err)
-		} else if mgmtNic == nil {
-			log.Debugf("can not get mgmt nic info, skip to configure route")
-		} else if utils.GetNicForRoute(server.CALLBACK_IP) != "eth0" {
-			err := utils.SetVrouterRoute(server.CALLBACK_IP, "eth0", "")
-			utils.PanicOnError(err)
-		} else {
-			log.Debugf("the cidr of vr mgmt contains callback ip, skip to configure route")
-		}
-		server.CURRENT_CALLBACK_IP = server.CALLBACK_IP
-	}
+	// 	mgmtNic := utils.GetMgmtInfoFromBootInfo()
+	// 	if mgmtNic == nil || utils.CheckMgmtCidrContainsIp(server.CALLBACK_IP, mgmtNic) == false {
+	// 		err := utils.SetVrouterRoute(server.CALLBACK_IP, "eth0", mgmtNic["gateway"].(string))
+	// 		utils.PanicOnError(err)
+	// 	} else if mgmtNic == nil {
+	// 		log.Debugf("can not get mgmt nic info, skip to configure route")
+	// 	} else if utils.GetNicForRoute(server.CALLBACK_IP) != "eth0" {
+	// 		err := utils.SetVrouterRoute(server.CALLBACK_IP, "eth0", "")
+	// 		utils.PanicOnError(err)
+	// 	} else {
+	// 		log.Debugf("the cidr of vr mgmt contains callback ip, skip to configure route")
+	// 	}
+	// 	server.CURRENT_CALLBACK_IP = server.CALLBACK_IP
+	// }
 }
 
 func init() {
